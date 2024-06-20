07035a26bb80c540fd49be5f092b84452994f0bb
Damaipatria <damaipatriasuyana10@gmail.com>
2024-06-20 23:08:48 +0700

    add card context in Card.tsx

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { createContext, useContext, useState } from "react"
 // component import
 import CardHeader from "../CardHeader"
 import CardFooter from "../CardFooter"
 import CardBody from "../CardBody"
+// import { CardContext, CardProvider } from "../Context/CardContext"
 
 type CardProps = {
   // style?: React.CSSProperties,
@@ -19,16 +20,40 @@ type CardProps = {
 // }
 // size = 'md'
 
+interface CardContextType {
+  imgOverlays: any
+  updateImgOverlays: (imgOverlays: any) => void
+}
+
+export const CardContext = createContext<CardContextType | null>(null)
+
+export const CardProvider = ({ children }: { children: React.ReactNode }) => {
+  const [imgOverlays, setImgOverlays] = useState<CardContextType>({
+    imgOverlays: false,
+    updateImgOverlays: (imgOverlays: any) => setImgOverlays(imgOverlays)
+  })
+
+  return (
+    <CardContext.Provider value={imgOverlays}>
+      {children}
+    </CardContext.Provider>
+  )
+}
+
 const Card = ({ children, width, heigth }: CardProps) => {
 
   // const sizeClass = sizes[size]
   // const widthString: string = `w-[${width}]`
+  const cardContext = useContext(CardContext)
 
   return (
     <>
-      <div className={`flex flex-col ${width ? width : "w-80"} ${heigth ? heigth : "h-min-52"} border rounded-xl shadow-md`}>
-        {children}
-      </div>
+      <CardProvider>
+        <div className={`flex flex-col ${width ? width : "w-80"} ${heigth ? heigth : "h-min-52"} border rounded-xl shadow-md`}>
+          {children}
+          <p>{cardContext ? 'true' : 'false'}</p>
+        </div>
+      </CardProvider>
     </>
   )
 }
--- src/CardImage/CardImage.tsx
+++ src/CardImage/CardImage.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useContext } from "react"
+import { CardContext } from "../Card/Card"
 
 export type CardProps = {
   src: string
@@ -7,9 +8,12 @@ export type CardProps = {
 
 const CardImage = ({ src, alt }: CardProps) => {
 
+  const cardContext = useContext(CardContext)
+
   return (
     <>
       <img src={src} alt={alt} className="w-full rounded-t-xl" />
+      <button className="text-white bg-blue-500 hover:bg-blue-600 active:bg-blue-700" onClick={() => { cardContext?.updateImgOverlays(true) }}>change imgOverlays value</button>
     </>
   )
 }
